b8e32de5999ff37c7eb5f7582e70297827a66ab3
Neha <neha.patel1995@gmail.com>
2019-04-20 23:41:51 +0100

    Trying to fix FindFriends

--- app/src/main/java/com/example/bookworm/HomeFragment.java
+++ app/src/main/java/com/example/bookworm/HomeFragment.java
@@ -4,18 +4,32 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class HomeFragment extends Fragment implements View.OnClickListener {
 
     private Button CreateClub;
     private View view;
-    private TextView txt_link_search_book;
+    private TextView FullName;
+
+    private DatabaseReference  UserRef ;
+    private FirebaseAuth mAuth;
+
+    private String currentUserID;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -26,9 +40,36 @@ public class HomeFragment extends Fragment implements View.OnClickListener {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
 
         view = inflater.inflate(R.layout.fragment_home, container, false);
+        // Inflate the layout for this fragment
+
+        mAuth = FirebaseAuth.getInstance();
+        currentUserID = mAuth.getCurrentUser().getUid();
+        UserRef = FirebaseDatabase.getInstance().getReference().child("Users").child(currentUserID);
+
+        FullName = (TextView) view.findViewById(R.id.txt_Profile_Fullname);
+
+
+        UserRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()){
+
+                    String Username = dataSnapshot.child("fullname").getValue().toString();
+
+                    FullName.setText("Welcome: "+ Username);
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+
 
         CreateClub = view.findViewById(R.id.btn_create_club);
         CreateClub.setOnClickListener(this);
--- app/src/main/java/com/example/bookworm/ProfileActivity.java
+++ app/src/main/java/com/example/bookworm/ProfileActivity.java
@@ -3,9 +3,12 @@ package com.example.bookworm;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -13,15 +16,19 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import javax.annotation.meta.When;
+
 public class ProfileActivity extends AppCompatActivity {
 
     private TextView Genre, FullName, Book;
     private Button Send, Decline;
 
-    private DatabaseReference ProfileUserRef, UserRef ;
+    private DatabaseReference FriendRequestRef, UserRef ;
     private FirebaseAuth mAuth;
 
     private String SendUserID, passUserID;
+
+    private String CURRENTSTATE;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,12 +36,13 @@ public class ProfileActivity extends AppCompatActivity {
         setContentView(R.layout.activity_profile);
 
         mAuth = FirebaseAuth.getInstance();
-        passUserID = getIntent().getExtras().get("Memeber_ID").toString();
+        SendUserID = mAuth.getCurrentUser().getUid(); //Gets the current users ID
+
+        FriendRequestRef = FirebaseDatabase.getInstance().getReference().child("FriendRequest");
+
+        passUserID = getIntent().getExtras().get("Memeber_ID").toString(); //Gets the ID of the member the current user is looking that
         UserRef = FirebaseDatabase.getInstance().getReference().child("Users");
 
-
-//        SendUserID = mAuth.getCurrentUser().getUid();
-//        ProfileUserRef = FirebaseDatabase.getInstance().getReference().child("Users").child(SendUserID);
 
 //Declaring variables
         Genre = (TextView) findViewById(R.id.txt_Profile_Genre);
@@ -43,6 +51,10 @@ public class ProfileActivity extends AppCompatActivity {
 
         Send = (Button)  findViewById(R.id.btnSend_Request);
         Decline = (Button) findViewById(R.id.btnCancel_Request);
+
+
+
+        CURRENTSTATE= "Not_Friends";
 
         UserRef.child(passUserID).addValueEventListener(new ValueEventListener() {
             @Override
@@ -56,6 +68,8 @@ public class ProfileActivity extends AppCompatActivity {
                     FullName.setText(Username);
                     Book.setText(Bookname);
                     Genre.setText(Genrename);
+
+                    ButtonText();
                 }
             }
 
@@ -65,6 +79,84 @@ public class ProfileActivity extends AppCompatActivity {
             }
         });
 
+        Decline.setVisibility(View.INVISIBLE);
+        Decline.setEnabled(false);
 
+        if(!SendUserID.equals(passUserID))
+         {
+            Send.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Send.setEnabled(false);
+
+                    if(CURRENTSTATE.equals("Not_Friends"))
+                    {
+                        SendFriendRequest();
+                    }
+                }
+            });
+
+        }else //Sets the send and decline buttons to invisible if the user is looking up their own profile
+            {
+                Decline.setVisibility(View.INVISIBLE);
+                Send.setVisibility(View.INVISIBLE);
+            }
+
+
+    }
+
+    private void ButtonText() {
+        FriendRequestRef.child(SendUserID)
+                .addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        if(dataSnapshot.hasChild(passUserID)){
+                            String request_type = dataSnapshot.child(passUserID).child("request_type")
+                                    .getValue().toString();
+
+                            if (request_type.equals("sent")){
+
+                                CURRENTSTATE = "request_type";
+                                Send.setText("Cancel Friend Request");
+                            }
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+    }
+
+    private void SendFriendRequest() {
+
+        FriendRequestRef.child(SendUserID).child(passUserID)
+                .child("request_type").setValue("sent") //Creating a new child node
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task)
+                    {
+                        if(task.isSuccessful())
+
+  //When a friend request is send the database request two different child (node) Stores the sender ID and the member ID for who the request is sent too
+                        {
+                            FriendRequestRef.child(passUserID).child(SendUserID)
+                                    .child("request_type").setValue("received").addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if (task.isSuccessful()){
+                                        Send.setEnabled(true);
+                                        CURRENTSTATE = "request_sent";
+                                        Send.setText("Cancel Friend Request");
+
+                                        Decline.setVisibility(View.INVISIBLE);
+                                        Decline.setEnabled(false);
+                                    }
+                                }
+                            });
+                        }
+                    }
+                });
     }
 }
